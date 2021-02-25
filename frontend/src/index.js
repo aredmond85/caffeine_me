11ee104e1b7060f36612215f566733dd5939b8c6
Austin Redmond <aredmond85@gmail.com>
2021-02-25 16:45:12 -0600

    refactoring

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,27 @@ const form = document.getElementById("newDrinkForm")
 const darkButton = document.getElementById("dark-button")
 const allDrinks = []
 
+// Adds Dark Mode to application //
+darkButton.addEventListener("click", () => {
+    if (document.body.style.backgroundColor === "") {
+        document.body.style.backgroundColor = "black";
+        document.querySelector("h1").style.color = "white";
+        document.getElementById("dark-button").className = "btn btn-light";
+        document.getElementById("dark-button").value = "Light Mode";
+        document.querySelectorAll("label").forEach(element =>
+            element.style.color = "white"
+        )
+    } else {
+        document.body.style.backgroundColor = ""
+        document.querySelector("h1").style.color = "black";
+        document.getElementById("dark-button").className = "btn btn-dark";
+        document.getElementById("dark-button").value = "Dark Mode";
+        document.querySelectorAll("label").forEach(element =>
+            element.style.color = "black"
+        )
+    }
+})
+
 // Event listener for form and adding a new Drink to the API //
 form.addEventListener("submit", (event) => {
 
@@ -40,23 +61,3 @@ api.fetchDrinks()
     )
 
 
-// Adds Dark Mode to application //
-darkButton.addEventListener("click", () => {
-    if (document.body.style.backgroundColor === "") {
-        document.body.style.backgroundColor = "black";
-        document.querySelector("h1").style.color = "white";
-        document.getElementById("dark-button").className = "btn btn-light";
-        document.getElementById("dark-button").value = "Light Mode";
-        document.querySelectorAll("label").forEach(element =>
-            element.style.color = "white"
-        )
-    } else {
-        document.body.style.backgroundColor = ""
-        document.querySelector("h1").style.color = "black";
-        document.getElementById("dark-button").className = "btn btn-dark";
-        document.getElementById("dark-button").value = "Dark Mode";
-        document.querySelectorAll("label").forEach(element =>
-            element.style.color = "black"
-        )
-    }
-})
