3bea8831bac71ed4d9f35f6275f31e91cd6f66fb
Austin Redmond <aredmond85@gmail.com>
2021-02-25 16:22:32 -0600

    removed DOMContentLoaded, was not needed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,42 +6,39 @@ const form = document.getElementById("newDrinkForm")
 const darkButton = document.getElementById("dark-button")
 const allDrinks = []
 
-document.addEventListener("DOMContentLoaded", function () {
+// Event listener for form and adding a new Drink to the API //
+form.addEventListener("submit", (event) => {
 
-    // Event listener for form and adding a new Drink to the API //
-    form.addEventListener("submit", (event) => {
-        event.preventDefault()
+    let name = event.target.name.value
+    let caffeine = event.target.caffeine.value
 
-        let name = event.target.name.value
-        let caffeine = event.target.caffeine.value
+    api.addDrink(name, caffeine)
+        .then(drink => {
+            const newDrink = new Drink(drink)
+            allDrinks.push(newDrink)
+        })
 
-        api.addDrink(name, caffeine)
-            .then(drink => {
-                const newDrink = new Drink(drink)
-                allDrinks.push(newDrink)
-            })
-
-        event.target.reset()
-    })
-
-    api.fetchDrinks()
-        .then(data => {
-            data.forEach(drink => {
-                const newDrink = new Drink(drink)
-                allDrinks.push(newDrink)
-            })
-        }).then(() =>
-            api.fetchComments()
-            .then(comments => {
-                comments.forEach(comment => {
-                    const newComment = new Comment(comment)
-                    const drink = Drink.findById(newComment.drinkId)
-                    drink.comments.push(newComment)
-                    drink.card.handleLoadComment()
-                })
-            })
-        )
+    event.target.reset()
 })
+
+api.fetchDrinks()
+    .then(data => {
+        data.forEach(drink => {
+            const newDrink = new Drink(drink)
+            allDrinks.push(newDrink)
+        })
+    }).then(() =>
+        api.fetchComments()
+        .then(comments => {
+            comments.forEach(comment => {
+                const newComment = new Comment(comment)
+                const drink = Drink.findById(newComment.drinkId)
+                drink.comments.push(newComment)
+                drink.card.handleLoadComment()
+            })
+        })
+    )
+
 
 // Adds Dark Mode to application //
 darkButton.addEventListener("click", () => {
